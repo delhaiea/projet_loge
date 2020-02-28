5d19b70e63cee4f396f07eecd9c242fd9ed232d0
Alexis Delhaie <alexis.delhaie@yahoo.com>
2020-02-28 15:02:52 +0100

    Switch to Kotlin

---
+++ app/src/main/java/fr/insset/projectloge2/MainActivity.kt
@@ -0,0 +1,58 @@
+package fr.insset.projectloge2
+
+import android.app.Activity
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import androidx.databinding.DataBindingUtil
+import fr.insset.projectloge2.databinding.ActivityMainBinding
+import net.gotev.uploadservice.protocols.multipart.MultipartUploadRequest
+
+class MainActivity : AppCompatActivity() {
+
+    inner class PhotoHandler {
+        fun onClickSelectPhoto(v: View) {
+            pickFile()
+        }
+    }
+
+    val photoChoosePicker = 2;
+
+    private lateinit var layout: ActivityMainBinding;
+    private lateinit var photoHandler: PhotoHandler;
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        layout = DataBindingUtil.setContentView(this, R.layout.activity_main);
+        photoHandler = PhotoHandler();
+        layout.photoHandler = photoHandler;
+    }
+
+    fun pickFile() {
+        val intent = Intent(Intent.ACTION_OPEN_DOCUMENT).apply {
+            addCategory(Intent.CATEGORY_OPENABLE)
+            type = "image/*"
+        }
+        startActivityForResult(intent, photoChoosePicker)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        if (requestCode == photoChoosePicker && resultCode == Activity.RESULT_OK) {
+            data?.let {
+                onFilePicked(it.data.toString())
+            }
+        } else {
+            super.onActivityResult(requestCode, resultCode, data)
+        }
+    }
+
+    private fun onFilePicked(filePath: String) {
+        MultipartUploadRequest(this, serverUrl = "https://ptsv2.com/t/ji5a9-1582895613/post")
+            .setMethod("POST")
+            .addFileToUpload(
+                filePath = filePath,
+                parameterName = "photo"
+            ).startUpload()
+    }
+}
